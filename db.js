9dea88632e0211ddfac884e1599064c640770d5e
Kristjan-Even <kristjan.puusepp@voco.ee>
2023-01-31 13:30:28 +0200

    Added handlebars template engine

---
+++ db.js
@@ -0,0 +1,34 @@
+const  express = require ('express')
+const app = express()
+
+const path = require('path')
+
+const hbs = require("express-handlebars");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine("hbs", hbs.engine({
+    extname: 'hbs',
+    defaultLayout: "main",
+    layoutsDir: __dirname + '/views/layouts/',
+}))
+
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:true}))
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "qwerty",
+    database: "joga_mysql"
+})
+
+con.connect(function (err) {
+    if (err) throw  err;
+    console.log("Connected to joga_mysql db");
+})
+app.listen(3000, () => {
+    console.log("App is starrted at http://localhost:3000");
+})
+
+
